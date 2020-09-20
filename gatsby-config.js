0c0feb57c83c91b5021cb8d2b0d59d72482c4ff8
jkvora <vorajalak@yahoo.co.in>
2020-09-20 10:46:06 +0530

    add gastby-theme

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+// gatsby-config.js
+
+module.exports = {
+    siteMetadata: {
+        title: `Novela by Narative`,
+        name: `Narative`,
+        siteUrl: `https://gatsby-theme-novela.netlify.com`,
+        description: `This is my description that will be used in the meta tags and important for search results`,
+    
+        // important to set the main text that appears in the hero
+        hero: {
+          heading: `Perspectives on technology, design and business from the team at Narative.`,
+          maxWidth: 652,
+        },
+        social: [
+          {
+            name: `twitter`,
+            url: `https://twitter.com/narative`,
+          },
+          {
+            name: `github`,
+            url: `https://github.com/narative`,
+          },
+        ],
+      },
+    plugins: ['@narative/gatsby-theme-novela'],
+  };
